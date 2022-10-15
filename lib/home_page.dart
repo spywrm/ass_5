c0131b11eee98adfe95b6f811a70932f9f81b01d
admin <kiewyom_w@silpakorn.edu>
2022-10-16 00:27:29 +0700

    1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,10 +8,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> listname = ["AAA", "BBB", "CCC"];
+  List<String> listname = [
+    "ทำการบ้านครั้งที่ 4 วิชา Moblide App Dev ",
+    "ทำการบ้านครั้งที่ 5 วิชา Moblie App Dev",
+    "ทำโปรเจควิชา Mobile App Dev"
+  ];
   List<bool> list = [true, true, true];
-  List<bool> check = [false,false,false];
-  Widget MyBox(index){
+  List<bool> check = [true, false, false];
+  final controller = TextEditingController();
+
+  void addButton() {
+    setState(() {
+      list.add(true);
+      check.add(false);
+      var name = controller.text;
+      listname.add(name);
+      //  print(controller.text);
+      // print(list.length);
+    });
+  }
+
+  Widget MyBox(index) {
     return Padding(
       padding: const EdgeInsets.all(5.0),
       child: Container(
@@ -23,21 +40,30 @@ class _HomeState extends State<Home> {
             Container(
               child: Row(
                 children: [
-                  Checkbox(value: check[index],onChanged: (newBool){
-                    setState(() {
-                      check[index] = newBool!;
-                    });
-                  },),
-                  Text(listname[index]),
+                  Checkbox(
+                    value: check[index],
+                    onChanged: (newBool) {
+                      setState(() {
+                        check[index] = newBool!;
+                      });
+                    },
+                  ),
+                  check[index] == false
+                      ? Text("${listname[index]}",style: TextStyle(fontSize: 17),)
+                      : Text("${listname[index]}",
+                          style: TextStyle(
+                            fontSize: 18,
+                              decoration: TextDecoration.lineThrough)),
+                  //Text(listname[index]),
                 ],
               ),
             ),
             InkWell(
-              onTap:(){
+              onTap: () {
                 setState(() {
-                  list[index]=false;
+                  list[index] = false;
                 });
-              } ,
+              },
               child: Icon(Icons.clear),
             ),
           ],
@@ -45,54 +71,73 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-  Widget BoxEmpty(){
+
+  Widget BoxEmpty() {
     return Container();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My ToDo")),
-        body: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, index) {
-            if(list[index]){
-              return MyBox(index);
-            }
-            return BoxEmpty();
-          },
-
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Checkbox(
+              value: true,
+              onChanged: (newBool) {
+                setState(() {
+                });
+              },
+            ),
+            SizedBox(width: 10),
+            Text("My ToDo",style: TextStyle(fontSize: 25),)
+          ],
         ),
-        bottomNavigationBar: BottomAppBar(
-         child: Row(
-           children: [
-             Expanded(
-               child: Container(
-                 height: 70,
+      ),
+      body: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          if (list[index]) {
+            print(index);
+            print('print contrainer');
+            return MyBox(index);
+          }
+          return BoxEmpty();
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 70,
                 // color: Colors.pink,
-                 alignment: Alignment.center,
-                 child:  Padding(
-                   padding: const EdgeInsets.all(10.0),
-                   child: TextField(
-                     decoration: const InputDecoration(
-                       border: OutlineInputBorder(),
-                       hintText: 'Enter new ToDo',
-                     ),
-                   ),
-                 ),
-               ),
-             ),
-             InkWell(
-               onTap: (){},
-               child: Container(
-                 alignment: Alignment.center,
-                 width: 100,
-                 height: 70,
-                 child: Text("ADD",style: TextStyle(color: Colors.blue,fontSize: 15)),
-               ),
-             ),
-           ],
-         ),
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: controller,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter new ToDo',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: addButton,
+              child: Container(
+                alignment: Alignment.center,
+                width: 100,
+                height: 70,
+                child: Text("ADD",
+                    style: TextStyle(color: Colors.blue, fontSize: 15)),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
